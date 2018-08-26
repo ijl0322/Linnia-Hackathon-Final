ffbad33157ebadd101d3a85631c83be14e6f3bb4
Gabriel Tan <gabriel.v.tan@gmail.com>
2018-08-26 15:55:58 -0400

    Updated with Bootstrap

--- src/components/Permissions/ViewAddPermission.js
+++ src/components/Permissions/ViewAddPermission.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Button from 'react-bootstrap/lib/Button';
 
 class ViewAddPermission extends Component {
 
--- src/components/Records/ViewAddRecord.js
+++ src/components/Records/ViewAddRecord.js
@@ -1,8 +1,30 @@
 import React, {Component} from 'react'
+import { Form, Input, Message, Button } from 'semantic-ui-react'
+
 
 
 class ViewAddRecord extends Component {
+    state = { name: '', creditScore: '', taxForm: '', errorMessage: '', loading: false };
+    
+    onInputChange = property => event => {
+        //this.props.clearInputError();
+        const value = event.target.value;
+        console.log(value);
+    
+        this.setState({ [property]: value });
+      };
+    
+      onSubmit = async event => {
+        event.preventDefault();
+        this.setState({ loading: true, errorMessage: '' });
+    
+        console.log("++++ ViewAddRecordForm.hadleSubmit name, creditScore ++++++");
+        console.log(this.state.name);
+        console.log(this.state.creditScore);
+        console.log(this.state.taxForm);
 
+        this.setState({ name: "", creditScore: "", taxForm: "", loading: false });
+      };
     render () {       
         return (
             <div>
@@ -12,17 +34,23 @@ class ViewAddRecord extends Component {
             </div>
           
           <div className="container">
-            <form>
+          <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
               <div className="form-group row">
                 <label className="col-sm-2 col-form-label">Name</label>
                 <div className="col-sm-10">
-                  <p className="form-control-static">John Smith</p>
+                  <p className="form-control-static"><Input
+                    value={this.state.name}
+                    onChange={event => this.setState({ name: event.target.value })}    
+                    labelPosition="left"/></p>
                 </div>
               </div>
               <div className="form-group row">
                 <label className="col-sm-2 col-form-label">Credit Score</label>
                 <div className="col-sm-10">
-                  <p className="form-control-static">750</p>
+                  <p className="form-control-static"><Input
+                    value={this.state.creditScore}
+                    onChange={event => this.setState({ creditScore: event.target.value })}    
+                    labelPosition="left"/></p>
                 </div>
               </div>
                 <div class="form-group">
@@ -30,7 +58,11 @@ class ViewAddRecord extends Component {
                     <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp"/>
                     <small id="fileHelp" class="form-text text-muted">Please upload your most recent tax form</small>
                 </div>
-            </form>
+                <Message error content={this.state.errorMessage} />
+                <Button primary loading={this.state.loading}>
+                Add New Record
+                </Button>
+            </Form>
           </div>
           </div>
         )
